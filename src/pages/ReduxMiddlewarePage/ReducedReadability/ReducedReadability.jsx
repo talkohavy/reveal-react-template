03b041d6726ac0d8e4728be66411714f8a7f069c
Tal Kohavy <talkohavy@gmail.com>
2024-05-05 09:12:06 +0300

    created the ReducedReadability component

---
+++ src/pages/ReduxMiddlewarePage/ReducedReadability/ReducedReadability.jsx
@@ -0,0 +1,28 @@
+import SideNotes from '../../../components/PresentationSlides/SideNotes';
+import Slide from '../../../components/PresentationSlides/Slide';
+
+export default function ReducedReadability() {
+  return (
+    <Slide>
+      <h3>Reduced Readability</h3>
+
+      <div className='r-stretch w-full border'>
+        <h2 className='self-center underline'>what else?</h2>
+
+        <ul className='p-4 text-left text-lg'>
+          <li className='fragment'>Micro-Services</li>
+          <li className='fragment'>Micro-Frontends</li>
+        </ul>
+      </div>
+
+      <SideNotes>
+        What else has reduced readability, and yet we still use it? (click: micro-services) Do we really think that
+        putting an API-gateway in the middle, which proxies our requests, is more readable? Same thing with
+        micro-frontends (click: micro-frontends), it is very complex, very hard to reason about when first starting, but
+        we use it because in the long run, the advantages overweigh the disadvantages. And if a programmer comes along
+        and says "hey man, this is too complex, I can't understand this", we tell him "You're a programmer, learn how to
+        fish".
+      </SideNotes>
+    </Slide>
+  );
+}
