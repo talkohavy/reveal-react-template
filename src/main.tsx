118d5c2ebb332d8dd6edb9959d42621a37137aa0
Tal Kohavy <talkohavy@gmail.com>
2025-07-17 09:12:03 +0300

    converted all files extensions to ts/tsx from js/jsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import DarkThemeProvider from './providers/DarkThemeProvider';
 
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <DarkThemeProvider>
-        <App />
-      </DarkThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+function Client() {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <DarkThemeProvider>
+          <App />
+        </DarkThemeProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+
+// );
+
+const rootElement = document.getElementById('root')!;
+const root = createRoot(rootElement);
+root.render(<Client />);
