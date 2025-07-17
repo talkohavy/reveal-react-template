e43df4d510cc6bad579e79301e76b0fa00adb912
Tal Kohavy <talkohavy@gmail.com>
2025-07-17 07:27:56 +0300

    removed DAL

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import axios from 'axios';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { initDAL } from './DAL';
 import DarkThemeProvider from './providers/DarkThemeProvider';
 
 import './index.css';
-
-const httpClientAxios = axios.create({ baseURL: 'http://localhost:8000', withCredentials: true });
-
-initDAL(httpClientAxios);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
