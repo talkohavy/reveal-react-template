66a76cb9aa464154ca8d421f18e99d131419a4a7
Tal Kohavy <talkohavy@gmail.com>
2025-07-18 10:18:10 +0300

    renamed to REVEAL_CLASSES

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -34,7 +34,7 @@ export const ANIMATION_CLASSES = {
   strikeThrough: 'strike-through', // Strike through
 };
 
-export const TEXT_CLASSES = {
+export const REVEAL_CLASSES = {
   /**
    * The `r-fit-text` class makes text as large as possible without overflowing the slide.
    * This is great when you want BIG text without having to manually find the right font
